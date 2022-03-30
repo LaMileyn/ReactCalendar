6d6e4d04ed7b867d19a366b2089ade9da2baf274
LaMileyn <lost-is-died@mail.ru>
2022-03-30 20:37:47 +0500

    Completed 1 version

--- src/App.js
+++ src/App.js
@@ -1,30 +1,168 @@
-
 import './App.css';
 import moment from 'moment';
 import {Header} from "./components/Header";
 import {Monitor} from "./components/Monitor";
 import {Calendar} from "./components/Calendar";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import videoo from './videos/vidos.MOV'
+import styled from "styled-components";
 
 
+const FormPositionWrapper = styled('div')`
+  position: absolute;
+  z-index: 100;
+  background-color: rgba(0, 0, 0, 0.35);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const FormWrapper = styled('div')`
+  width: 250px;
+  background-color: #1E1F21;
+  color: #DDDDDD;
+  box-shadow: unset;
+  border-radius: 10px;
+`
+const EventTittle = styled('input')`
+  padding: 11px 14px;
+  font-size: .85rem;
+  width: 100%;
+  border: unset;
+  background-color: #1E1F21;
+  color: #DDDDDD;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  border-bottom: 1px solid #464648;
+`
+const EventBody = styled('input')`
+  padding: 11px 14px;
+  font-size: .85rem;
+  width: 100%;
+  border: unset;
+  background-color: #1E1F21;
+  color: #DDDDDD;
+  border-bottom: 1px solid #464648;
+`
+const ButtonsWrapper = styled('div')`
+  padding: 8px 14px;
+  display: flex;
+  justify-content: flex-end;
+`
+const Button = styled('button')`
+  padding: 4px 7px;
+  border: unset;
+  background-color: #2e2e31;
+  color: #fff;
+  cursor: pointer;
+  margin-right: 7px;
+  border-radius: 3px;
+
+`
+const defaultEvent = {
+    title: "",
+    description: "",
+    date: moment().format("X")
+}
 
 function App() {
 
-    moment.updateLocale('en', { week : {dow : 1}})
-    const [today,setToday] = useState(moment())
+    moment.updateLocale('en', {week: {dow: 1}})
+    const [today, setToday] = useState(moment())
+    const [event, setEvent] = useState(null)
+    const [events, setEvents] = useState([])
+    const [openForm, setOpenForm] = useState(false)
+    const [currenMethod, setCurrentMethod] = useState("")
     const startDay = today.clone().startOf('month').startOf('week');
-    const prevHandler = () => setToday( prev => prev.clone().subtract(1,'month'))
+    const prevHandler = () => setToday(prev => prev.clone().subtract(1, 'month'))
     const todayHandler = () => setToday(moment())
-    const nextHandler = () => setToday( prev => prev.clone().add(1,'month'))
+    const nextHandler = () => setToday(prev => prev.clone().add(1, 'month'))
 
 
-  return (
-    <section className="calendar">
-      <Header/>
-      <Monitor today = {today} prevHandler = {prevHandler} todayHandler={todayHandler} nextHandler ={nextHandler} />
-      <Calendar startDay = {startDay} currTime = {today}/>
-    </section>
-  );
+    const startDateQuery = startDay.clone().format("X")
+    const endDateQuery = startDay.clone().add(42, "days").format("X")
+
+    const openFormHandler = (method, eventToUpdate) => {
+        setEvent(eventToUpdate || defaultEvent)
+        setOpenForm(true)
+        setCurrentMethod(method)
+    }
+
+
+    useEffect(async () => {
+        const res = await fetch(`http://localhost:5000/events?date_gte=${startDateQuery}&date_lte=${endDateQuery}`);
+        const data = await res.json();
+        setEvents(data)
+    }, [today])
+
+    const cancelButtonHandler = () => {
+        setOpenForm(false)
+    }
+
+    const changeFieldValuesHandler = (text, field) => {
+        setEvent(prev => (
+            {
+                ...prev,
+                [field]: text
+            }
+        ))
+    }
+    const eventFetchHandler = () => {
+        const fetchUrl = currenMethod === "Update" ? `http://localhost:5000/events/${event.id}` : `http://localhost:5000/events`;
+        const httpMethod = currenMethod === "Update" ? "PATCH" : "POST";
+        fetch(fetchUrl, {
+            method: httpMethod,
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(event)
+        })
+            .then(response => response.json())
+            .then(res => {
+                cancelButtonHandler()
+                if ( currenMethod === "Create"){
+                    setEvents( prev => [...prev,res])
+                }else{
+                    setEvents( prev => prev.map( el => el.id === res.id ? res : el))
+                }
+                console.log(res)
+            })
+    }
+
+    return (
+        <section className="calendar">
+            {
+                openForm
+                    ? (
+                        <FormPositionWrapper onClick={cancelButtonHandler}>
+                            <FormWrapper onClick={e => e.stopPropagation()}>
+                                <EventTittle
+                                    onChange={e => changeFieldValuesHandler(e.target.value, "title")}
+                                    value={event.title} placeholder="Title"></EventTittle>
+                                <EventBody
+                                    onChange={e => changeFieldValuesHandler(e.target.value, "description")}
+                                    value={event.description} placeholder="Description"
+                                ></EventBody>
+                                <ButtonsWrapper>
+                                    <Button onClick={cancelButtonHandler}>Cancel</Button>
+                                    <Button onClick={eventFetchHandler}>{currenMethod}</Button>
+                                </ButtonsWrapper>
+                            </FormWrapper>
+                        </FormPositionWrapper>
+                    )
+                    : null
+            }
+            <Header/>
+            <Monitor
+                today={today}
+                prevHandler={prevHandler}
+                todayHandler={todayHandler} nextHandler={nextHandler}/>
+            <Calendar startDay={startDay} currTime={today} events={events} openFormHandler={openFormHandler}/>
+        </section>
+    );
 }
 
 export default App;
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -6,18 +6,19 @@ const GridWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   background-color: #404040;
-  gap: ${ props => props.months ? '0px' : "1px"};;
+  gap: ${props => props.months ? '0px' : "1px"};;
 `
 const CellWrapper = styled.div`
   min-width: 140px;
-  min-height: ${ props => props.months ? '24px' : "88px"};
-  border-bottom:${ props => props.months ? '1px solid #404040' : "0"}; ;
+  min-height: ${props => props.months ? '24px' : "88px"};
+  border-bottom: ${props => props.months ? '1px solid #404040' : "0"};;
   background-color: ${props => props.weekend ? "#272829" : "#1E1F21"};
-  color: ${ props => props.selectedMonth ? ( props.weekend ? "gray":  "#DDDCDD" ) : "#555759"};
+  color: ${props => props.selectedMonth ? (props.weekend ? "gray" : "#DDDCDD") : "#555759"};
 `
 const RowInCell = styled.div`
   display: flex;
-  padding-right: ${ props => props.padding ? '8px' : 0};
+  flex-direction: column;
+  padding-right: ${props => props.padding ? '8px' : 0};
   justify-content: ${(props) => props.justifyContent ? props.justifyContent : "flex-start"};
 `
 const DayWrapper = styled.div`
@@ -27,6 +28,7 @@ const DayWrapper = styled.div`
   align-items: center;
   justify-content: center;
   margin: 2px;
+  cursor: pointer;
 `
 
 const ActiveDate = styled.div`
@@ -38,8 +40,31 @@ const ActiveDate = styled.div`
   align-items: center;
   justify-content: center;
 `
-
-const Calendar = ({startDay,currTime}) => {
+const ShowDayWrapper = styled('div')`
+  display: flex;
+  justify-content: flex-end;
+`
+const EventListWrapper = styled('ul')`
+  margin: unset;
+  list-style-position: inside;
+  padding-left: 4px;
+`
+const EventItemWrapper = styled('button')`
+  position: relative;
+  left: -14px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 114px;
+  border: unset;
+  background: unset;
+  color: #DDDDDD;
+  cursor: pointer;
+  padding: 0;
+  text-align: left;
+  margin: 0;
+`
+const Calendar = ({startDay, currTime, events,openFormHandler}) => {
     const totalDays = 6 * 7;
     const day = startDay.clone().subtract(1, 'day');
     const daysArray = [...Array(42)].map(() => day.add(1, 'day').clone())
@@ -51,10 +76,12 @@ const Calendar = ({startDay,currTime}) => {
     return (
         <>
             <GridWrapper months>
-                {[...Array(7)].map( (_,i) => (
-                    <CellWrapper months selectedMonth = {true} key = {i}>
-                        <RowInCell justifyContent={'flex-end'} padding >
-                            {moment().day(i+1).format("ddd")}
+                {[...Array(7)].map((_, i) => (
+                    <CellWrapper months selectedMonth={true} key={i}>
+                        <RowInCell justifyContent={'flex-end'} padding>
+                            <ShowDayWrapper>
+                                {moment().day(i + 1).format("ddd")}
+                            </ShowDayWrapper>
                         </RowInCell>
                     </CellWrapper>
                 ))}
@@ -64,23 +91,37 @@ const Calendar = ({startDay,currTime}) => {
                     <CellWrapper
                         key={dayItem.unix()}
                         weekend={dayItem.day() === 6 || dayItem.day() === 0}
-                        selectedMonth = {checkTheMonth(dayItem)}
+                        selectedMonth={checkTheMonth(dayItem)}
                     >
                         <RowInCell justifyContent={"flex-end"}>
-                            <DayWrapper>
-                                {
-                                    checkTheDay(dayItem)
-                                        ? (<ActiveDate>{dayItem.format("D")}</ActiveDate>)
-                                        : dayItem.format("D")
-                                }
+                            <ShowDayWrapper>
+                                <DayWrapper onClick = { () => openFormHandler("Create")}>
+                                    {
+                                        checkTheDay(dayItem)
+                                            ? (<ActiveDate>{dayItem.format("D")}</ActiveDate>)
+                                            : dayItem.format("D")
+                                    }
 
-                            </DayWrapper>
+                                </DayWrapper>
+                            </ShowDayWrapper>
+                            <EventListWrapper>
+                                {
+                                    events?.filter(event => event.date >= dayItem.format("X") && event.date <= dayItem.clone().endOf('day').format("X"))
+                                        .map(event => (
+                                            <li key={event.id}>
+                                                <EventItemWrapper onClick = { () => openFormHandler("Update",event)}>
+                                                    {event.title}
+                                                </EventItemWrapper>
+                                            </li>
+                                        ))
+                                }
+                            </EventListWrapper>
                         </RowInCell>
                     </CellWrapper>
                 ))}
             </GridWrapper>
-            </>
+        </>
 
-            )
-            }
+    )
+}
 export {Calendar};
